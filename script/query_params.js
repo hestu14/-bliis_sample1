019341f5a003634b4c36eb76a130f8bd7ec82de2
hestu14 <hestu.14@gmail.com>
2020-09-24 06:45:30 +0700

    still draft

---
+++ script/query_params.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    try {
+        var url_string = (window.location.href);
+        var url = new URL(url_string);
+        var nama = url.searchParams.get("nama");
+        var alamat = url.searchParams.get("alamat");
+
+        var tujuan;
+
+        if (nama === null || alamat === null) {
+            tujuan = "............";
+            // $( "#tujuan-isi" ).hide();
+        } else {
+            tujuan = nama + " (" + alamat + ")";
+        }
+
+        document.getElementById("tujuan-isi").innerHTML = tujuan;
+
+    } catch (err) {
+        console.log("Issues with Parsing URL Parameter's - " + err);
+    }
+}
